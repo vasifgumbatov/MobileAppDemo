092d3fbc59b96fce7cb93a7c3827ba06d52c399e
Talibstarter <ul0270132@edu.uni.lodz.pl>
2024-12-31 01:29:17 +0400

    Revert "18.11.2024 -> Integrate CredentialsManager into LoginActivity"
    This reverts commit 2ce416f339aeac46195334207f8c19b98a50925f.

--- app/src/main/java/com/vasifgumbatov/newapplication/ui/signinscreen/SignInFragment.kt
+++ app/src/main/java/com/vasifgumbatov/newapplication/ui/signinscreen/SignInFragment.kt
@@ -44,7 +44,7 @@ class SignInFragment : CoreFragment<FragmentSignInBinding>() {
         when {
             !isValidEmail(email) -> binding?.inputTextSignIn?.error = "Invalid email format"
             password.length < 4 -> binding?.inputPasswordSignIn?.error = "Password must be at least 4 characters"
-            email == "test@te.st" && password == "1234" -> navigateToExample()
+            email == "test@te.st" && password == "1234" -> navigateToMain()
             else -> {
                 binding?.inputTextSignIn?.error = "Invalid email or password"
                 binding?.inputPasswordSignIn?.error = "Invalid email or password"
@@ -52,8 +52,10 @@ class SignInFragment : CoreFragment<FragmentSignInBinding>() {
         }
     }
 
-    private fun navigateToExample() {
-        findNavController().navigate(R.id.action_signInFragment_to_exampleFragment)
+    private fun navigateToMain() {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
     }
 
     private fun isValidEmail(email: String): Boolean {
